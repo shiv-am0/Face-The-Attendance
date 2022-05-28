fb3d1fe71102d6254e1cdbbb8eb0b39209f60fd9
shiv-am0 <shivam.362000.1@gmail.com>
2022-05-28 11:37:36 +0530

    Camera Integration working

---
+++ app/src/main/java/com/sriv/shivam/facetheattendance/camera/CameraHelper.kt
@@ -0,0 +1,198 @@
+package com.sriv.shivam.facetheattendance.camera
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.pm.PackageManager
+import android.util.DisplayMetrics
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.mlkit.vision.barcode.Barcode
+import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.common.InputImage
+import java.lang.Math.abs
+import java.lang.Math.max
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class CameraHelper(
+    private val owner: AppCompatActivity,
+    private val context: Context,
+    private val viewFinder: PreviewView,
+    private val onResult: (result: String) -> Unit
+) {
+    private var cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
+
+    private var lensFacing: Int = CameraSelector.LENS_FACING_BACK
+    private var camera: Camera? = null
+    private var cameraProvider: ProcessCameraProvider? = null
+
+
+    fun start() {
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(owner, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+    }
+
+    fun stop() {
+        cameraExecutor.shutdown()
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+        cameraProviderFuture.addListener({
+
+            cameraProvider = cameraProviderFuture.get()
+
+            lensFacing = when {
+                hasFrontCamera() -> CameraSelector.LENS_FACING_FRONT
+                hasBackCamera() -> CameraSelector.LENS_FACING_BACK
+                else -> throw IllegalStateException("Back and front camera are unavailable")
+            }
+            bindCameraUseCases()
+        }, ContextCompat.getMainExecutor(context))
+    }
+
+    private fun bindCameraUseCases() {
+
+        val cameraProvider = cameraProvider ?: throw IllegalStateException("Camera initialization failed.")
+
+        val cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+        val previewView = getPreviewUseCase()
+//        val barcodeRecognizer = getBarcodeAnalyzerUseCase(onResult)
+
+        cameraProvider.unbindAll()
+
+        try {
+            camera = cameraProvider.bindToLifecycle(
+                owner,
+                cameraSelector,
+                previewView,
+            )
+
+            previewView.setSurfaceProvider(viewFinder.surfaceProvider)
+
+        } catch (exc: Exception) {
+            Log.e(TAG, "Use case binding failed $exc")
+        }
+    }
+
+    private fun aspectRatio(): Int {
+        with(DisplayMetrics().also { viewFinder.display.getRealMetrics(it) }) {
+            val previewRatio = max(widthPixels, heightPixels).toDouble() / widthPixels.coerceAtMost(
+                heightPixels
+            )
+            if (abs(previewRatio - RATIO_4_3_VALUE) <= abs(previewRatio - RATIO_16_9_VALUE)) {
+                return AspectRatio.RATIO_4_3
+            }
+            return AspectRatio.RATIO_16_9
+        }
+    }
+
+    private fun hasBackCamera() =
+        cameraProvider?.hasCamera(CameraSelector.DEFAULT_BACK_CAMERA) ?: false
+
+    private fun hasFrontCamera() =
+        cameraProvider?.hasCamera(CameraSelector.DEFAULT_FRONT_CAMERA) ?: false
+
+    private fun getPreviewUseCase(): Preview {
+        return Preview.Builder()
+            .setTargetAspectRatio(aspectRatio())
+            .setTargetRotation(viewFinder.display.rotation)
+            .build()
+    }
+
+//    private fun getBarcodeAnalyzerUseCase(onResult: (result: String) -> Unit): ImageAnalysis {
+//        val analyzer = ImageAnalysis.Builder()
+//            .setTargetAspectRatio(aspectRatio())
+//            .setTargetRotation(viewFinder.display.rotation)
+//            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+//            .setImageQueueDepth(10)
+//            .build()
+//
+//        analyzer.setAnalyzer(cameraExecutor, BarcodeScanner { string ->
+//            string?.let { url ->
+//                onResult(url)
+//            }
+//        })
+//
+//        return analyzer
+//    }
+
+//    private class BarcodeScanner(listener: BarcodeListener? = null) : ImageAnalysis.Analyzer {
+//        private val listeners = ArrayList<BarcodeListener>().apply { listener?.let { add(it) } }
+//
+//        val options = BarcodeScannerOptions.Builder()
+//            .setBarcodeFormats(Barcode.FORMAT_ALL_FORMATS)
+//            .build()
+//
+////        @SuppressLint("UnsafeExperimentalUsageError")
+////        override fun analyze(imageProxy: ImageProxy) {
+////            val mediaImage = imageProxy.image
+////            if (mediaImage != null) {
+////                val image =
+////                    InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+////                val scanner = BarcodeScanning.getClient(options)
+////                scanner.process(image)
+////                    .addOnSuccessListener { barcodes ->
+////                        barcodes.forEach { barcode ->
+////                            listeners.forEach { it(barcode.displayValue) }
+////                        }
+////                        imageProxy.close()
+////                    }
+////                    .addOnFailureListener {
+////                        imageProxy.close()
+////                    }
+////            }
+////        }
+//    }
+
+    fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(
+                    context,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    //Function to check whether all permissions required for camera are granted by the user or not
+    private fun allPermissionsGranted(): Boolean {
+        for (permission in REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(
+                    context, permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return false
+            }
+        }
+        return true
+    }
+
+    companion object {
+        const val REQUEST_CODE_PERMISSIONS = 42
+        val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val RATIO_4_3_VALUE = 4.0 / 3.0
+        private const val RATIO_16_9_VALUE = 16.0 / 9.0
+    }
+}
--- app/src/main/java/com/sriv/shivam/facetheattendance/fragments/AttendanceCameraFragment.kt
+++ app/src/main/java/com/sriv/shivam/facetheattendance/fragments/AttendanceCameraFragment.kt
@@ -6,18 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import com.google.common.util.concurrent.ListenableFuture
 import com.sriv.shivam.facetheattendance.R
+import com.sriv.shivam.facetheattendance.camera.CameraHelper
+import com.sriv.shivam.facetheattendance.databinding.ActivityMainBinding
 import com.sriv.shivam.facetheattendance.databinding.FragmentAttendanceCameraBinding
 
 class AttendanceCameraFragment : Fragment() {
     lateinit var binding: FragmentAttendanceCameraBinding
     private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
     private lateinit var cameraSelector: CameraSelector
+    private lateinit var cameraHelper: CameraHelper
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,29 +34,57 @@ class AttendanceCameraFragment : Fragment() {
         cameraProviderFuture = ProcessCameraProvider.getInstance(requireActivity())
         cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
-        startCamera()
+        cameraHelper = context?.let {
+            CameraHelper(
+                owner = activity as AppCompatActivity,
+                context = it,
+                viewFinder = binding.camera,
+                onResult = ::onResult
+            )
+        }!!
+
+        cameraHelper.start()
+
+//        startCamera()
 
         return binding.root
     }
 
-    private fun startCamera() {
-        // listening for data from the camera
-        cameraProviderFuture.addListener({
-            val cameraProvider = cameraProviderFuture.get()
-
-            // connecting a preview use case to the preview in the xml file.
-            val preview = Preview.Builder().build().also{
-                it.setSurfaceProvider(binding.camera.surfaceProvider)
-            }
-            try{
-                // clear all the previous use cases first.
-                cameraProvider.unbindAll()
-                // binding the lifecycle of the camera to the lifecycle of the application.
-                cameraProvider.bindToLifecycle(this,cameraSelector,preview)
-            } catch (e: Exception) {
-                Log.d("TAG", "Use case binding failed")
-            }
-
-        }, ContextCompat.getMainExecutor(requireActivity()))
+    private fun onResult(result: String) {
+        Log.d(TAG, "Result is $result")
+        Toast.makeText(context, "Result", Toast.LENGTH_SHORT).show()
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        cameraHelper.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    companion object {
+        const val TAG = "CameraXDemo"
+    }
+
+//    private fun startCamera() {
+//        // listening for data from the camera
+//        cameraProviderFuture.addListener({
+//            val cameraProvider = cameraProviderFuture.get()
+//
+//            // connecting a preview use case to the preview in the xml file.
+//            val preview = Preview.Builder().build().also{
+//                it.setSurfaceProvider(binding.camera.surfaceProvider)
+//            }
+//            try{
+//                // clear all the previous use cases first.
+//                cameraProvider.unbindAll()
+//                // binding the lifecycle of the camera to the lifecycle of the application.
+//                cameraProvider.bindToLifecycle(this,cameraSelector,preview)
+//            } catch (e: Exception) {
+//                Log.d("TAG", "Use case binding failed")
+//            }
+//
+//        }, ContextCompat.getMainExecutor(requireActivity()))
+//    }
 }
